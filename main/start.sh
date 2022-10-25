bafd236a73262e01093d0d7b372bf6df5694bbe9
dcsdiu <98796064+dcsdiu@users.noreply.github.com>
2022-10-25 01:31:16 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
-export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export PORT=${PORT-7877}
+export UUID=${UUID-cc43b3fefac94447bab65aad68c59085}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
